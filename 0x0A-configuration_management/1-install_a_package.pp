faf29560a6eda9f80906c43f1bbb0e4bd1c3c4ce
abdennuur <abdnourkhatib@gmail.com>
2024-03-22 13:42:30 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
-# 1-install_a_package.pp
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
+# install flask from pip3
+package {'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    }
