bde75ea77a4ee90d77bf7074e97343ca2efa73ed
abdennuur <abdnourkhatib@gmail.com>
2024-05-07 06:05:23 -0700

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit.
+queries Reddit API and print the title of the
+first 10 hot posts listed for given subreddit.
 """
 
 import requests
@@ -9,19 +9,19 @@ import requests
 
 def top_ten(subreddit):
     """
-    Function that queries the Reddit API
-    - If not a valid subreddit, print None.
+    Queries the Reddit API
+    - If not valid subreddit, None.
     """
-    req = requests.get(
+    reQ = requests.get(
         "https://www.reddit.com/r/{}/hot.json".format(subreddit),
         headers={"User-Agent": "Custom"},
         params={"limit": 10},
     )
 
-    if req.status_code == 200:
-        for get_data in req.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
+    if reQ.status_code == 200:
+        for get_dt in reQ.json().get("data").get("children"):
+            dt = get_dt.get("data")
+            title = dt.get("title")
             print(title)
     else:
         print(None)
