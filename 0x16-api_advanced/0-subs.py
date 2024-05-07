d74e956a5a89c1cdb1612674f3bfb48a92feed5d
abdennuur <abdnourkhatib@gmail.com>
2024-05-07 05:04:00 -0700

    0-

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
-"""To query subscribers on given Reddit subreddit"""
+"""
+0-subs
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return -> total number of subscribers on given subreddit"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    rspns = requests.get(url, headers=headers, allow_redirects=False)
-    if rspns.status_code == 404:
+    """Returns the number of subscribers for a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Custom User Agent'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
         return 0
-    rslts = rspns.json().get("data")
-    return rslts.get("subscribers")
+
+
+if __name__ == "__main__":
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
