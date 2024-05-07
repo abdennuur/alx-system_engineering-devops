7d4048ed7303a8c003a61e6966c5bebd10bb3255
abdennuur <abdnourkhatib@gmail.com>
2024-05-07 04:58:35 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,16 @@
+#!/usr/bin/python3
+"""To query subscribers on given Reddit subreddit"""
 import requests
 
-def number_of_subscribers(subreddit):
-    """Queries Reddit API and returns the number of subscribers for a given subreddit."""
-    # Input validation
-    if not subreddit or not isinstance(subreddit, str):
-        raise ValueError("Subreddit name must be a non-empty string.")
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    
-    response = requests.get(url, headers=headers)
-    
-    if response.status_code == 200:
-        data = response.json()
-        return data.get('data', {}).get('subscribers', 0)
-    else:
-        print(f"Failed to fetch data for subreddit '{subreddit}'. Error: {response.text}")
+def number_of_subscribers(subreddit):
+    """Return -> total number of subscribers on given subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    rspns = requests.get(url, headers=headers, allow_redirects=False)
+    if rspns.status_code == 404:
         return 0
+    rslts = rspns.json().get("data")
+    return rslts.get("subscribers")
