b69bd4b49d739353d6cc9712204361ed0ae11780
abdennuur <abdnourkhatib@gmail.com>
2024-05-07 05:40:57 -0700

    fix

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,6 +3,8 @@
 To count words in all hot posts of given Reddit subreddit.
 """
 import requests
+
+
 def count_words(subreddit, word_list, after=None, counts={}):
     """
     Recursive function to queries Reddit API, parsestitle of all
@@ -10,28 +12,31 @@ def count_words(subreddit, word_list, after=None, counts={}):
     """
     if not word_list or word_list == [] or not subreddit:
         return
+
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {"User-Agent": "Mozilla/5.0"}
+
     params = {"limit": 100}
     if after:
         paramts["after"] = after
+
     response = requests.get(url,
                             headers=headers,
                             params=params,
                             allow_redirects=False)
+
     if response.status_code != 200:
         return
 
     data = response.json()
-    children = data["data"]["children"]
     chldrn = data["data"]["children"]
 
-    for post in children:
     for post in chldrn:
         title = post["data"]["title"].lower()
         for word in word_list:
             if word.lower() in title:
                 counts[word] = counts.get(word, 0) + title.count(word.lower())
+
     after = data["data"]["after"]
     if after:
         count_words(subreddit, word_list, after, counts)
