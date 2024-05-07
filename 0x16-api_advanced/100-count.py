3d2b643c4277a7b010b1a5c85b24015da71b4db9
abdennuur <abdnourkhatib@gmail.com>
2024-05-07 05:43:32 -0700

    fix

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -41,7 +41,7 @@ def count_words(subreddit, word_list, after=None, counts={}):
     if after:
         count_words(subreddit, word_list, after, counts)
     else:
-        sorted_counts = sorted(counts.items(),
+        sorted_Cnts = sorted(counts.items(),
                                key=lambda x: (-x[1], x[0].lower()))
-        for word, count in sorted_counts:
+        for word, count in sorted_Cnts:
             print(f"{word.lower()}: {count}")
