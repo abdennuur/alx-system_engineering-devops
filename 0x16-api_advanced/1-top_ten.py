73fba42875ad6d8af32eeefdf8df16a95a704560
abdennuur <abdnourkhatib@gmail.com>
2024-05-07 05:14:16 -0700

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+To queries Reddit API and print titles of
+first 10 hot posts listed for given subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    TOqueries the Reddit API
+    - If not a valid subreddit -> print None.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10},
+    )
+
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            Dat = get_data.get("data")
+            title = Dat.get("title")
+            print(title)
+    else:
+        print(None)
