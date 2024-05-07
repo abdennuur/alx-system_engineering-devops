7b2ef72bd1029662ff2215a2f37a9ae2e0a46e37
abdennuur <abdnourkhatib@gmail.com>
2024-05-07 04:49:43 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,10 +9,10 @@ def number_of_subscribers(subreddit):
     """queries Reddit API and return number of
     subscribers for given subreddit"""
     uRl = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    heaDers = {'User-Agent': 'My User Agent 1.0'}
-    resP = requests.get(uRl, heaDers=headers)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    resP = requests.get(uRl, headers=headers)
     if resP.status_code == 200:
-        daTa = resP.json()
-        return daTa.get('data').get('subscribers')
+        data = resP.json()
+        return data.get('data').get('subscribers')
     else:
         return 0
