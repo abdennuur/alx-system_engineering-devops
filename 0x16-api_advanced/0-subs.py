6f5665c6005b8645fd065b26146748c560040542
abdennuur <abdnourkhatib@gmail.com>
2024-05-09 04:24:07 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!_usr/bin/python3
 """
 TO queries subscribers on given Reddit subreddit.
 """
@@ -7,13 +7,21 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return -> total number of subscribers on given subreddit"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-        rslt = response.json().get("data")
-        return rslt.get(subscribers)
+    """Return the total number of subscribers for a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+
+    # Check if the response was successful
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data.get('data', {}).get('subscribers', 0)
+        return subscribers
+    else:
+        # If the subreddit does not exist, Reddit returns a 404 status code
+        if response.status_code == 404:
+            return 0
+        else:
+            # Handle other potential errors by printing the status code
+            print("Error:", response.status_code)
+            return 0
