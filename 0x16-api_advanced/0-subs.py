2ff8eb6528a7aac0f08f2ce833ddbb7730f053c9
abdennuur <abdnourkhatib@gmail.com>
2024-05-09 04:05:33 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,20 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return -> total number of subscribers on given subreddit"""
+    """Return the total number of subscribers for a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # Check if response is a redirection indicating a non-existing subreddit
+    if response.status_code == 302:
+        return 0
+
+    # To Check if response is successful and retrieve the subscriber count
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
-    else:
-        return 0
+
+    # Return -> 0 for any other cases (e.g., errors)
+    return 0
